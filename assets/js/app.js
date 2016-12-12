77ad1ca2dcb9531a233799dcc7fa3490fcad0736
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-11 21:27:35 -0700

    getCoordinates func with denver test pass

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  $("form").on("submit", function(address) {
-    event.preventDefault();
+
     // $.get("https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&format=json&lat=39.7392358&lon=-104.990251", function(data) {
     //   console.log(data);
     // });
-
-    "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBecXcSD1TtOEr_uAXkjPsiqG8dRTsMsA0&address=Denver,+CO"
-  });
+  getCoordinates();
 });
 
 function getCoordinates() {
-
+  $("form").on("submit", function(address) {
+    event.preventDefault();
+    $.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBecXcSD1TtOEr_uAXkjPsiqG8dRTsMsA0&address=Denver,+CO", function(data) {
+      var lat = data.results[0].geometry.location.lat;
+      var lng = data.results[0].geometry.location.lng;
+      console.log("lat is " + lat);
+      console.log("long is " + lng);
+    });
+  });
 }
