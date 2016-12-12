d44b98622e2248283ececc38bfe3e2f6f7899e21
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-12 11:47:19 -0700

    changed data style and format

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $("form").on("submit", function() {
     event.preventDefault();
     getCoordinates();
-
   });
 });
 
@@ -15,9 +14,8 @@ function getCoordinates() {
     var lat = data.results[0].geometry.location.lat;
     var lng = data.results[0].geometry.location.lng;
     $(".solar_data_1").html("");
-    $(".solar_data_1").append("<li>"+loc+"</li>");
-    $(".solar_data_1").append("<li class='lat'>latitude: "+lat+"</li>");
-    $(".solar_data_1").append("<li class='long'>longitude: "+lng+"</li>");
+    $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>place</i>"+loc+"</div><div class='collapsible-body'><ul><li class='lat'>Latitude: "+lat+"</li><li class='long'>Longitude: "+lng+"</li></ul></div></li>");
+    $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>assessment</i>"+"solar resources"+"</div><div class='collapsible-body'><ul class='solar_data_2'></ul></div></li>");
     getSolarData();
   });
 }
@@ -31,11 +29,12 @@ function getSolarData() {
   var long_result = long_string.substring(11, long_string.length);
   $.get("https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&format=json&lat="+lat_result+"&lon="+long_result, function(data) {
     var outputs = data.outputs;
+
     var dni = outputs.avg_dni.annual;
-    $(".solar_data_1").append("<li class='dni'>Annual avg dni: "+dni+"</li>");
+    $(".solar_data_2").append("<li class='dni collection-item'>Annual avg dni: "+dni+"</li>");
     var ghi = outputs.avg_ghi.annual;
-    $(".solar_data_1").append("<li class='ghi'>Annual avg ghi: "+ghi+"</li>");
+    $(".solar_data_2").append("<li class='ghi collection-item'>Annual avg ghi: "+ghi+"</li>");
     var lat_tilt = outputs.avg_lat_tilt.annual;
-    $(".solar_data_1").append("<li class='lat_tilt'>Annual avg lat-tilt: "+lat_tilt+"</li>");
+    $(".solar_data_2").append("<li class='lat_tilt collection-item'>Annual avg lat-tilt: "+lat_tilt+"</li>");
   });
 }
