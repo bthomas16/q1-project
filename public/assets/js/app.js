2c37badfe309811e311ea2b4c8ff9de3a43b78b5
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-12 18:26:28 -0700

    added popular city selection on state click

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,24 +2,32 @@ $(document).ready(function() {
   $("form").on("submit", function() {
     event.preventDefault();
     getCoordinates();
+    $('html, body').animate({
+        scrollTop: $("#user_data_solar").offset().top
+    }, 2000);
   });
 });
 
 function getCoordinates() {
   var city = $("#location").val();
   var state = $(".state_selected").text();
+  var City = toTitleCase(city);
+  var State = toTitleCase(state);
+  
   $.get("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBecXcSD1TtOEr_uAXkjPsiqG8dRTsMsA0&address="+city+"+"+state, function(data) {
     var lat = data.results[0].geometry.location.lat;
     var lng = data.results[0].geometry.location.lng;
     $(".solar_data_1").html("");
-    $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>place</i>"+city+"</div><div class='collapsible-body'><ul><li class='lat'>Latitude: "+lat+"</li><li class='long'>Longitude: "+lng+"</li></ul></div></li>");
+    $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>place</i>"+City+", "+State+"</div><div class='collapsible-body'><ul><li class='lat'>Latitude: "+lat+"</li><li class='long'>Longitude: "+lng+"</li></ul></div></li>");
     $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>assessment</i>"+"solar resources"+"</div><div class='collapsible-body'><ul class='solar_data_2'></ul></div></li>");
     getSolarData();
     getSolarProd(lat, lng);
   });
 }
 
-
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
 function getSolarData() {
   var lat_string = $(".lat").html();
@@ -28,7 +36,6 @@ function getSolarData() {
   var long_result = long_string.substring(11, long_string.length);
   $.get("https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&format=json&lat="+lat_result+"&lon="+long_result, function(data) {
     var outputs = data.outputs;
-
     var dni = outputs.avg_dni.annual;
     $(".solar_data_2").append("<li class='dni collection-item'>Annual avg dni: "+dni+"</li>");
     var ghi = outputs.avg_ghi.annual;
--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
     $(".state_feed").html($(this).attr('id'));
   });
   $(".hexagon").click(function() {
-    $(".state_selected").html($(this).attr('id'));
+    var state = $(this).attr('id');
+    $(".state_selected").html(state);
     $("#location").val("");
+    city_list = cities[state];
+    $(".chip_city").html("");
+    for (var i = 0; i < city_list.length; i++) {
+      $(".chip_city").append("<div class='chip hoverable'>"+city_list[i]+"</div>");
+    }
   });
 });
+
+var cities = { Colorado: ["Denver", "Boulder", "Fort Collins", "Colorado Springs"] };
