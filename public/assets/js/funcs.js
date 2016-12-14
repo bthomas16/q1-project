b242e8fe810d697d3354fd66be40dd287c7dc4d6
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2016-12-14 11:06:50 -0700

    added utility rates functionality

--- public/assets/js/funcs.js
+++ public/assets/js/funcs.js
@@ -1,9 +1,12 @@
+
+// Mouseover functionality of the state selection
 function state_mouse_over() {
   $(".hexagon").on("mouseover", function() {
     $(".state_feed").html($(this).attr('id'));
   });
 }
 
+// Click event functions on state selection
 function state_click() {
   $(".hexagon").click(function() {
 
@@ -19,7 +22,7 @@ function state_click() {
     // popular city object
     var cities = { Colorado: ["Denver", "Fort Collins", "Colorado Springs"],
     Alabama: ["Birmingham", "Montgomery", "Mobile"],
-    Alaska: ["Anchorage", "Juneau", "Fairbanks", "Sitka", "Ketchikan"],
+    Alaska: ["Anchorage", "Juneau", "Fairbanks"],
     Arizona: ["Phoenix", "Tucson", "Scottsdale"],
     Arkansas: ["Little Rock", "Fort Smith", "Fayetteville"],
     California: ["Los Angeles", "San Diego", "San Francisco"],
@@ -84,11 +87,19 @@ function state_click() {
     //city click function
     city_click();
     animateSearch();
-
-
   });
 }
 
+// Have search bar come up as user scrolls over popular cities
+function animateSearch() {
+  $(".button_city > a.btn").on("mouseover", function() {
+    $(".hidden_form").slideDown("slow", function() {
+
+    });
+  });
+}
+
+// click event triggered by user click on popular city
 function city_click() {
   $(".button_city > a.btn").click(function(event) {
     var city_selected = $(this).text();
@@ -98,14 +109,7 @@ function city_click() {
   });
 }
 
-function animateSearch() {
-  $(".button_city > a.btn").on("mouseover", function() {
-    $(".hidden_form").slideDown("slow", function() {
-      //$(".hidden_form").attr("style", "display: flex");
-    });
-  });
-}
-
+// Get coordinates from google maps api
 function getCoordinates(city, state) {
   var City = toTitleCase(city);
   var State = toTitleCase(state);
@@ -120,17 +124,21 @@ function getCoordinates(city, state) {
     // solar resource data appended
     $(".solar_data_1").append("<li><div class='collapsible-header'><i class='material-icons'>assessment</i>"+"Average Solar Resources"+"</div><div class='collapsible-body'><ul class='solar_data_2'></ul></div></li>");
     getSolarData();
-    getSolarProd(lat, lng);
+    utility_rates(lat, lng);
+    //getSolarProd(lat, lng);
     energy_incentives(lat, lng);
     $(".solar_card").removeClass("hidden_cards");
     scroll_down();
   });
 }
 
+
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+
+// GET SOLAR OVERVIEW DATA
 function getSolarData() {
   var lat_string = $(".lat").html();
   var lat_result = lat_string.substring(10, lat_string.length);
@@ -138,34 +146,67 @@ function getSolarData() {
   var long_result = long_string.substring(11, long_string.length);
   $.get("https://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&format=json&lat="+lat_result+"&lon="+long_result, function(data) {
     var outputs = data.outputs;
+
+    // get dni data
     var dni = outputs.avg_dni.annual;
+    // append dni data
     $(".solar_data_2").append("<li class='dni collection-item'>Annual avg dni: "+dni+"</li>");
+
+    // get ghi data
     var ghi = outputs.avg_ghi.annual;
+    // append ghi data
     $(".solar_data_2").append("<li class='ghi collection-item'>Annual avg ghi: "+ghi+"</li>");
+
+    // get lat tilt data
     var lat_tilt = outputs.avg_lat_tilt.annual;
+    // append lat tilt data
     $(".solar_data_2").append("<li class='lat_tilt collection-item'>Annual avg lat-tilt: "+lat_tilt+"</li>");
   });
 }
 
-function getSolarProd(lat, long) {
-  $(".kWh").remove();
+// GET UTILITY RATES
+function utility_rates(lat, long) {
+  $.get("https://developer.nrel.gov/api/utility_rates/v3.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&lat="+lat+"&lon="+long, function(data) {
+    var cost = data.outputs.residential;
+    var cost_str = cost.toString();
+    var cost_result = "$ " + cost_str + " / kWh";
+    $(".utility_cost").text(cost_result);
+
+    getSolarProd(lat, long, cost);
+    //  calculate monthly electricty value
+    // var arr = $(".ac_prod");
+    // for (var i = 0; i < arr.length; i++) {
+    //   var cur = arr[i];
+    //   var next = $(cur).next();
+    //   var value = cost
+    //   $(next).text()
+    // }
+    // var cost_string = $(".utility_cost").text();
+    // var cost = Number(cost_string.substring(2, 8));
+  });
+}
+
+// GET MONTHLY SOLAR POWER PRODUCTION VALUES
+function getSolarProd(lat, long, cost) {
   var arr = $(".ac_prod");
   $.get("https://developer.nrel.gov/api/pvwatts/v5.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&format=json&lat="+lat+"&lon="+long+"&system_capacity=5&module_type=1&losses=10&array_type=1&tilt=40&azimuth=180", function(data) {
     for (var i = 0; i < arr.length; i++) {
       var ac_data = Math.round(data.outputs.ac_monthly[i]);
       var current_item = arr[i];
       arr[i].innerHTML = ac_data;
+      var value = cost * ac_data;
+      value = value.toFixed(2);
+      var next = $(current_item).next();
+      $(next).text(value);
     }
   });
   $(".solar-prod-data").attr("style", "display: block");
 }
 
-function scroll_down() {
-  $('html, body').animate({
-      scrollTop: $("#user_data_solar").offset().top
-  }, 2000);
-}
 
+
+
+// GET REQUEST FOR SOLAR POWER FIN INCENTIVES
 function energy_incentives(lat, long) {
   $.get("https://developer.nrel.gov/api/energy_incentives/v2/dsire.json?api_key=ZJH76qOhbyarfoAWyLVAtsKgRcGm5bdna1qd7gjz&lat="+lat+"&lon="+long+"&category=solar_technologies&technology=solar_photovoltaics", function(data) {
     var master_result_obj = {};
@@ -182,16 +223,23 @@ function energy_incentives(lat, long) {
     }
 
     var fin_incents = master_result_obj["Financial Incentive"];
-    $(".solar_data_3").html("")
+    $(".solar_data_3").html("");
     for (var j = 0; j < fin_incents.length; j++) {
       cur_obj = fin_incents[j];
       var keys_arr = Object.keys(cur_obj);
       var cur_key = keys_arr[0];
       var cur_val = cur_obj[cur_key];
-      // console.log(cur_key);
-      // console.log(cur_val);
       var fin_data = $("<li><div class='collapsible-header'>"+cur_key+"</div><div class='collapsible-body'><div class='container'>"+cur_val+"</div></div></li>");
       $(".solar_data_3").append(fin_data);
     }
   });
+}
+
+
+
+// SCROLL DOWN AFTER LOCATION SUBMISSION
+function scroll_down() {
+  $('html, body').animate({
+    scrollTop: $("#user_data_solar").offset().top
+  }, 2000);
 }
